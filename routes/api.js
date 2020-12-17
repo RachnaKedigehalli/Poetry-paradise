6ee1d70681ba89e69a9798037b7e0d44fa0c0761
RachnaKedigehalli <rachna.kedigehalli@iiitb.org>
2020-12-17 20:16:03 +0530

    frontend updated

--- routes/api.js
+++ routes/api.js
@@ -5,69 +5,66 @@ const mongoose = require('mongoose');
 const Poem = require('../models/poem.js');
 const Interpretation = require('../models/interpretation.js');
 
+// search based on _id
+router.get('/poems/:id', function(request, response, next){
+    Poem.findOne({_id: request.params.id}).then(function(poem){
+        response.send(poem);
+    });
+});
 
+// search based on title, verse and poet on the poem
 router.post('/poems/search', function(request, response, next){
-    
+    // search through body of post request so that characters like new line are read properly
+
+    // finds all poems whose title has the substring request.body.title 
     var titleFind = async function(resArr) {
         if(request.body.title) {
-            console.log("test");
             Poem.find({title: { '$regex' : request.body.title, '$options' : 'i' }}).then(function(poem){
                 titleArr = Array.from(poem);
-                // console.log("\nTitle:\n");
-                // console.log(titleArr);
                 resArr = resArr.concat(titleArr);
                 verseFind(resArr);
             });
         }
         else verseFind(resArr);
-        // return resArr;
     };
+
+    // finds all poems whose verse has the substring request.body.verse
     var verseFind = async function(resArr) {
         if(request.body.verse) {
             Poem.find({verse: { '$regex' : request.body.verse, '$options' : 'i' }}).then(function(poem){
                 verseArr = Array.from(poem);
-                // console.log("\nverse:\n");
-                // console.log(verseArr);
                 resArr = resArr.concat(verseArr);
                 poetFind(resArr);
             });
         }
         else poetFind(resArr);
-        // return resArr;
     };
+    
+    // finds all poems whose poet has the substring request.body.poet 
     var poetFind = async function(resArr) {
         if(request.body.poet) {
             Poem.find({poet: { '$regex' : request.body.poet, '$options' : 'i' }}).then(function(poem){
                 poetArr = Array.from(poem);
-                // console.log("\nPoet:\n");
-                // console.log(poetArr);
                 resArr = resArr.concat(poetArr);
 
                 removeDuplicate(resArr);
             });
         }
         else removeDuplicate(resArr);
-        // return resArr;
     };
 
+    // resArr is concatanation of titleArr, verseArr and poetArr
+    // removes duplicates from resArr, if any
     var removeDuplicate = function(resArr) {
-        // console.log("\nTitle:\n");
-        //     console.log(titleArr);
-        //     console.log("\nverse:\n");
-        //     console.log(verseArr);
-        //     console.log("\nPoet:\n");
-        //     console.log(poetArr);
         for(var i=0; i<resArr.length; i++) {
             for(var j=i+1; j<resArr.length; j++) {
                 var arr = [i,j,String(resArr[i]._id)==String(resArr[j]._id)];
-                // console.log(arr);
                 if(String(resArr[i]._id)==String(resArr[j]._id)) {
                     resArr.splice(j,1);
                     j--;
                 }
             }
         }
-        // console.log(resArr+"---------------------------------------------------------------");
         response.send(resArr);
     };
     var titleArr = [];
@@ -77,12 +74,6 @@ router.post('/poems/search', function(request, response, next){
     titleFind(resArr);
 });
 
-// search based on _id
-router.get('/poems/:id', function(request, response, next){
-    Poem.findOne({_id: request.params.id}).then(function(poem){
-        response.send(poem);
-    });
-});
 
 // add poem to db
 router.post('/poems', function(request, response, next){
@@ -95,9 +86,13 @@ router.post('/poems', function(request, response, next){
 // add interpretation to poem db
 router.put('/poems/interpretations/:id', function(request, response, next){
     Poem.findOne({_id: request.params.id}).then(function(poem){
+        // creates interpretation object based on Schema
         const Itp = mongoose.model('interpretation', Interpretation);
         var itp = new Itp(request.body);
         var newObj = {};
+        
+        // adds interpretation object to existing array of interpretation objects 
+        // and updates poem details
         newObj.interpretations = poem.interpretations.concat(itp);
         Poem.findByIdAndUpdate({_id: request.params.id}, newObj).then(function(){
             Poem.findOne({_id: request.params.id}).then(function(poem){
